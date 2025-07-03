b6b23d0db94da2734977e520cfe6ea18e6157ec0
nihitha-ch <nihitha3103@gmail.com>
2025-07-04 01:14:04 +0530

    Restructure folders, and set up server

---
+++ chess-client/src/pages/Game/index.jsx
@@ -0,0 +1,62 @@
+import React, {useState, useRef, useEffect, useContext} from 'react';
+import Chess from 'chess.js';
+import {createBoard} from '../../functions/index.js';
+import Board from '../../components/board/index.jsx';
+import { GameContext } from '../../context/GameContext.js';
+import { types } from '../../context/actions.js';
+import getGameOverState from '../../functions/game-over.js';
+import GameOver from '../../components/gameover/index.jsx';
+
+const FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+const Game = ()=> {
+    const [fen, setFen] = useState(FEN);
+    const {current: chess}= useRef (new Chess(fen));
+    const [board, setBoard] = useState(createBoard(fen));
+    const { dispatch, gameOver } = useContext(GameContext);
+    
+    useEffect(()=> {
+        setBoard(createBoard(fen));
+    },[fen]);
+
+    useEffect(() => {
+      const [gameOver, status] = getGameOverState(chess);
+        if (gameOver) {
+            dispatch({ type: types.GAME_OVER, status, player: chess.turn() });
+            return;
+        }
+    dispatch({
+        type: types.SET_TURN, //import types from '../../context/actions'
+        player: chess.turn(),
+        check: chess.in_check(),
+    });
+}, [fen, dispatch, chess]);
+    const fromPos = useRef();
+
+    const makeMove = (pos) => {
+        const from = fromPos.current;
+        const to = pos;
+        chess.move({ from, to});
+        dispatch({ type: types.CLEAR_POSSIBLE_MOVES });
+        setFen(chess.fen());
+    
+    };
+
+    const setFromPos = (pos) => {
+      fromPos.current = pos;
+      dispatch({
+        type: types.SET_POSSIBLE_MOVES,
+        moves: chess.moves({ square: pos }),
+    });
+  };
+  if (gameOver) {
+		return <GameOver />;
+	}
+    return (
+      <div className="game">
+        <Board cells={board} makeMove={makeMove} setFromPos={setFromPos}/> 
+      </div>
+    );
+  
+};
+
+export default Game;
